b672e0d28f1969cf713ab555fca5c18ae5bf293e
Kirill-Eremeev <65193580+Kirill-Eremeev@users.noreply.github.com>
2020-11-21 23:15:29 +0300

    My first git-repo

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-print('My first git-repo')
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from random import randint
+
+
+class Prog(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 211, 0))
+        S = randint(10, 100)
+        qp.drawEllipse(300 - S / 2, 300 - S / 2, S, S)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    calc = Prog()
+    calc.show()
+    sys.exit(app.exec())
